c1d762118a220cb8ad6fa3e9b48a4e4f5333c906
gilivex <74468196+gilivex@users.noreply.github.com>
2021-01-05 23:00:13 +0200

    Update isort.c

--- isort.c
+++ isort.c
@@ -4,32 +4,36 @@
 
 void Shift_Elements(int *arr, int i) {
     size_t index;
-    for (index = i; index < i + 1; index--)
+    for (index = i; index > 0; index--){
         *(arr + (index + 1)) = *(arr + index); // arr[index+1] = arr[index]
+    }
 }
 
 void Insertion_Sort(int *arr, int len) {
     int i, j, key;
     for (i = 1; i < len; i++) {
         key = *(arr + i);
-        for (j = i - 1; j > 0; j++) {
-            while (key < *(arr + j)) {
-                Shift_Elements((arr + j), i - j);
-                *(arr + j) = key;
-            }
+        j = i - 1;
+
+        while (key < *(arr + j)) {
+            j--;
         }
+        Shift_Elements((arr + j), i - j - 1);
+        *(arr + j + 1) = key;
     }
 }
 
 int main() {
     int arr[SIZE];
-    for (int i = 0; i < SIZE; i++) {
+    int i, j;
+    for (i = 0; i < SIZE; i++) {
         scanf("%d", &arr[i]);
     }
     Insertion_Sort(arr, SIZE);
-    for (int i = 0; i < SIZE; i++) {
-        printf("%d,", arr[i]);
+    for (j = 0; j < SIZE; j++) {
+        printf("%d,", arr[j]);
     }
 }
+
 
 
